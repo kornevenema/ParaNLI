6d41e9d9d4d1bb103238fa0f646df398f551f1f8
kornevenema <kornevenema@hotmail.com>
2023-05-05 19:39:21 +0200

    added mnli test files

---
+++ main.py
@@ -0,0 +1,27 @@
+import json
+
+
+def extract_sentences_and_label(path, out_path):
+    with open(out_path, "w", encoding="utf-8") as out_f:
+        with open(path, encoding="utf-8") as f:
+            for id_, row in enumerate(f):
+                data = json.loads(row)
+                new_data = {"label": data["gold_label"],
+                            "premise": data["sentence1"],
+                            "hypothesis": data["sentence2"],
+                            }
+                json.dump(new_data, out_f)
+                out_f.write('\n')
+
+
+def main():
+    extract_sentences_and_label(
+        "data/original/multinli_0.9_test_mismatched_unlabeled_hard.jsonl",
+        "data/original/multinli_test_mismatched.jsonl")
+    extract_sentences_and_label(
+        "data/original/multinli_0.9_test_matched_unlabeled_hard.jsonl",
+        "data/original/multinli_test_matched.jsonl")
+
+
+if __name__ == '__main__':
+    main()
